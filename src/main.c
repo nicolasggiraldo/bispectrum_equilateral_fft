839fb5143f78194d475b61f4561dccddb666041f
nicolasggiraldo <nicolasggiraldo@gmail.com>
2016-09-21 16:16:30 -0500

    Se agrega columna Ntri al archivo de salida

--- src/main.c
+++ src/main.c
@@ -284,9 +284,9 @@ int main(int argc, char *argv[])
   fprintf(fout,"# HUBBLEPARAM    = %lf\n", GV.HUBBLEPARAM);
   //fprintf(fout,"\n");
   
-  fprintf(fout,"#%19s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s\n",
+  fprintf(fout,"#%19s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s %20s\n",
 	  "k1", "k2", "k3", "P(k1)", "P(k2)", "P(k3)", "B(k1,k2,k3)", "Q(k1,k2,k3)",
-	  "Error_P(k1)", "Error_P(k2)", "Error_P(k3)", "Error_B123", "Error_Q123");
+	  "Error_P(k1)", "Error_P(k2)", "Error_P(k3)", "Error_B123", "Error_Q123", "Ntri");
   
   /* Lineal binning */
   GV.K1 = GV.DELTA_K*0.5;
@@ -435,10 +435,10 @@ int main(int argc, char *argv[])
 			     (GV.Pk1+GV.Pk1)*GV.Pk1_Error);
       GV.Qk_Error /= ( GV.Pk1*GV.Pk1 + GV.Pk1*GV.Pk1 + GV.Pk1*GV.Pk1 );
       
-      fprintf(fout,"%20lf %20lf %20lf %20e %20e %20e %20e %20e %20e %20e %20e %20e %20e\n",
+      fprintf(fout,"%20lf %20lf %20lf %20e %20e %20e %20e %20e %20e %20e %20e %20e %20e %20e\n",
 	      GV.K1, GV.K1, GV.K1, GV.Pk1, GV.Pk1, GV.Pk1, GV.Bk, GV.Qk,
 	      GV.Pk1_Error, GV.Pk1_Error, GV.Pk1_Error,
-	      GV.Bk_Error, GV.Qk_Error);
+	      GV.Bk_Error, GV.Qk_Error, GV.Ntri);
       
       GV.K1 += (step*GV.DELTA_K);
       //GV.K1 += (0.5 * GV.DELTA_K);
